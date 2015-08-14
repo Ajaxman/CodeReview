513663715e9347fe609225b5a68269644fc7f7b5
Javier <javier.lopez@clickbus.com.mx>
2015-08-14 13:33:31 -0500

    The base structure to the project

---
+++ bin/app.php
@@ -0,0 +1,8 @@
+<?php
+//bin/app.php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Jlopez\CodeReview\Command\CodeSnifferCommand;
+
+$test = new CodeSnifferCommand();
+//$test
---
+++ src/Jlopez/CodeReview/Command/CodeSnifferCommand.php
@@ -0,0 +1,12 @@
+<?php
+namespace Jlopez\CodeReview\Command;
+/**
+ *
+ */
+class CodeSnifferCommand
+{
+  public function __construct()
+  {
+     echo "working...";         
+  }
+}
